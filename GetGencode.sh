8c2ea69ee0c4c9bd18deca7b43e1c927e28700fe
LTaing <lieng.taing@curie.fr>
2018-08-02 15:40:19 +0200

    Adding the conda env file\n conda create --name PROTEOMEGENERATOR --file PROTEOMEGENERATOR-file.txt

---
+++ GetGencode.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+## Torque Configuration
+#PBS -l walltime=24:00:00
+#PBS -l mem=50gb
+#PBS -l nodes=1:ppn=10
+#PBS -q batch
+#PBS -N Rsem_prepare
+#PBS -j oe
+
+GENECODE_VERSION=28
+GRCH_VERSION=38
+
+echo "`date` Rsem_prepare start"
+source "/data/users/ltaing/.bashrc"
+source activate PROTEOMEGENERATOR
+
+RSEM_RESSOURCES_DIR=/data/tmp/ltaing/SUPPA/RSEM/RESSOURCES/
+RSEM_INDEX_DIR=${RSEM_RESSOURCES_DIR}GENCODE/GencodeV${GENECODE_VERSION}/INDEX/
+mkdir --parents ${RSEM_INDEX_DIR}
+
+GENOME_URL="ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${GENECODE_VERSION}/GRCh${GRCH_VERSION}.primary_assembly.genome.fa.gz"
+GENOME="${RSEM_RESSOURCES_DIR}GENCODE/GencodeV${GENECODE_VERSION}/GRCh${GRCH_VERSION}.primary_assembly.genome.fa"
+
+if [ ! -f ${GENOME} ];
+then
+ wget ${GENOME_URL} \
+ --output-document ${GENOME}.gz
+ gunzip ${GENOME}.gz
+fi
+
+GTF_URL="ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${GENECODE_VERSION}/gencode.v${GENECODE_VERSION}.annotation.gtf.gz"
+GTF="${RSEM_RESSOURCES_DIR}GENCODE/GencodeV${GENECODE_VERSION}/gencode.v${GENECODE_VERSION}.annotation.gtf"
+
+if [ ! -f ${GTF} ];
+then
+ wget ${GTF_URL} \
+ --output-document ${GTF}.gz
+ gunzip ${GTF}.gz
+fi
+echo "`date` DL done"
+
+RSEM_INDEX_NAME=${RSEM_INDEX_DIR}GRCh${GRCH_VERSION}_x_gencode.v${GENECODE_VERSION}
+rsem-prepare-reference --gtf ${GTF} --star -p 10 ${GENOME} ${RSEM_INDEX_NAME}
+
